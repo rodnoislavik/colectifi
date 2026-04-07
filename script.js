22202595bfacc3619eb143fb2d5f560e405bcc24
rodnoislavik <slavavolkov730@gmail.com>
2026-04-07 10:06:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const publishBtn = document.querySelector('[data-type="publish"]');
+const viewBtn = document.querySelector('[data-type="view"]');
+
+if (publishBtn) publishBtn.href = "seller.html";
+if (viewBtn) viewBtn.href = "buyer.html";
